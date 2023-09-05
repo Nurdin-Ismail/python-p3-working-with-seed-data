98c6d2cfcd2a00083882d1dec9869d41014ee380
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-05 12:51:44 +0300

    debug and seed files and extra modifications on models

--- lib/debug.py
+++ lib/debug.py
@@ -17,3 +17,14 @@ if __name__ == '__main__':
 
 
     import ipdb; ipdb.set_trace()
+
+    # botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    # session.add(botw)
+    # session.commit()
+
+    # ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    # mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    # session.bulk_save_objects([ffvii, mk8])
+    # session.commit()
+
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
-from faker import Faker
+# from faker import Faker
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Game
 
-fake = Faker()
+# fake = Faker()
 
 if __name__ == '__main__':
     
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    session.query(Game).delete()
+    session.commit()
+
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+
+
+    session.bulk_save_objects([botw, ffvii, mk8, ccs])
+    session.commit()
